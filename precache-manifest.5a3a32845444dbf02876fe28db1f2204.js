ace334270d00127ed09396e9d4b0a829f78af5e3
Hong10Jay <wangweijunjay@163.com>
2019-11-28 16:24:34 +0800

    Updates

---
+++ precache-manifest.5a3a32845444dbf02876fe28db1f2204.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f8064ef581161a85e8817c1480cea51",
+    "url": "/react-demo/index.html"
+  },
+  {
+    "revision": "067a5daa40230cdd0f31",
+    "url": "/react-demo/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "45bb47b34f24fcbaed38",
+    "url": "/react-demo/static/js/2.a02b0a50.chunk.js"
+  },
+  {
+    "revision": "067a5daa40230cdd0f31",
+    "url": "/react-demo/static/js/main.d6312198.chunk.js"
+  },
+  {
+    "revision": "2784aae79be0630cf950",
+    "url": "/react-demo/static/js/runtime-main.be8c1ffd.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/react-demo/static/media/logo.25bf045c.svg"
+  }
+]);
